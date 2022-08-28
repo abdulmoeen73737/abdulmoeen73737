999ae41624d0c484e28a8bee8e235cbae72fa4c3
abdulmoeen73737 <112026628+abdulmoeen73737@users.noreply.github.com>
2022-08-28 23:25:27 +0300

    Add files via upload

---
+++ Homework2.java
@@ -0,0 +1,153 @@
+import java.util.Scanner;
+
+//التكليف الثاني
+//Write a Java program to compute a specified formula.
+// Specified Formula:
+// 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
+//public class Homework2 {
+//    public static void main ( String [] args) {
+//    System.out.print(4.0*(1-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11)));
+//    }}
+//*************************************************************************************
+//Write a Java program to print the area and perimeter of a circle.
+//import java.util.Scanner;
+//public class Homework2 {
+//  public static void main(String[] args) {
+//  int r;
+//  double pi = 3.14, area;
+//  Scanner in=new Scanner(System.in);
+//  System.out.print("Enter radius of circle:");
+//  r=in.nextInt();
+//  area = pi * r * r;
+//  System.out.println("Area of circle:"+area);
+//  }}
+//***************************************************************
+//Write a Java program to print the area and perimeter of a rectangle
+//import java.util.Scanner;
+//public class Homework2 {
+//    public static void main(String[] args) {
+//    int length,width;
+//    Scanner in=new Scanner(System.in);
+//    length=in.nextInt();
+//    width=in.nextInt();
+//    System.out.print("input arae ");
+//    System.out.println(length*width);
+//    }}
+//***************************************************************
+//Write a Java program that takes three numbers as input to calculate and print the average of the numbers
+//import java.util.Scanner;
+//public class Homework2 {
+//    public static void main(String[] args){
+//        float  a,c,d;
+//        Scanner in=new Scanner(System.in);
+//        a=in.nextInt();
+//        c=in.nextInt();
+//        d=in.nextInt();
+//        System.out.print("this sum number is= ");
+//        System.out.println(a+c+d);
+//        System.out.print("ths  average is = ");
+//        System.out.println((a+c+d)/3);
+//    }}
+//***************************************************************
+//Write a Java program to swap two variables
+//public class Homework2 {
+//    public static void main(String[] args){
+//        int  c,d;
+//        Scanner in=new Scanner(System.in);
+//        System.out.print("enter  the value of  c= ");
+//        c=in.nextInt();
+//        System.out.print("enter  the value of  d= ");
+//        d=in.nextInt();
+//        System.out.print("enter  the value of c is : ");
+//        System.out.println(d);
+//        System.out.print("enter  the value of d is : ");
+//        System.out.print(c);
+//    }}
+//***************************************************************
+//What are the values of the following expressions? In each line, assume tha
+//public class Homework2 {
+//    public static void main(String[] args) {
+//        String s = "Hello";
+//        String t = "World";
+//        System.out.println(s.length() + t.length());
+//        System.out.println(s.substring(1, 2));
+//        System.out.println(s.substring(s.length() / 2, s.length()));
+//        System.out.println(s + t);
+//        System.out.println(t+s);
+//    }}
+//***************************************************************
+// How do you get the first character of a string?
+// The last character? How do you remove the first character? The last character?
+//public class Homework2 {
+//    public static void main(String[] args) {
+//        String s = "Hello";
+//        System.out.println(s.substring(0,1));
+////هكذا للحصول على الحرف الاول
+//        System.out.println(s.substring(1,5));
+//        //هكذا لازالة الحرف الاول
+//    }}
+//***************************************************************
+//Write a program that reads a number and displays the square, cube, and fourth power.
+// Use the Math.pow method only for the fourth power
+//import java.util.Scanner;
+//public class Homework2 {
+//    public static void main(String[] args) {
+//        int g;
+//        Scanner in=new Scanner(System.in);
+//        g=in.nextInt();
+//        System.out.print(" square=");
+//        System.out.println(Math.pow(g,2));
+//        System.out.print(" cube=");
+//        System.out.println(Math.pow(g,3));
+//        System.out.print("fourth=");
+//        System.out.println(Math.pow(g,4));
+//    }}
+//***************************************************************
+//Write a program that prompts the user for two
+// integers and then prints  • The sum  • The difference • The product•The average•The distance.The maximum .the minimum
+//import java.util.Scanner;
+//public class Homework2 {
+//    public static void main(String[] args) {
+//        int b,d;
+//        Scanner in=new Scanner(System.in);
+//        b=in.nextInt();
+//        d=in.nextInt();
+//        System.out.print("sum=");
+//        System.out.println(b+d);
+//        System.out.print("difference=");
+//        System.out.println(b-d);
+//        System.out.print("avg=");
+//        System.out.println((b+d)/2);
+//        System.out.print("maximum=");
+//        System.out.println(Math.max(b,d));
+//        System.out.print("minimum=");
+//        System.out.println(Math.min(b,d));
+//        System.out.print("distance=");
+//        System.out.println(Math.abs(b-d));
+//    }}
+//***************************************************************
+//10. Enhance the output of Exercise E4.4 so that the numbers are properly aligned:
+//    public class Homework2 {
+//    public static void main(String[] args) {
+//        int b=20,d=25;
+//        System.out.print("sum=");
+//        System.out.println(b+d);
+//        System.out.print("difference=");
+//        System.out.println(b-d);
+//        System.out.print("porduct=");
+//        System.out.println(b*d);
+//        System.out.print("avg=");
+//        System.out.println((b+d)/2.0);
+//        System.out.print("maximum=");
+//        System.out.println(Math.max(b,d));
+//        System.out.print("minimum=");
+//        System.out.println(Math.min(b,d));
+//        System.out.print("distance=");
+//        System.out.println(Math.abs(b-d));
+//    }}
+//***************************************************************
+//public class Homework2 {
+//    public static void main(String [] ards )
+//    {
+//   System.out.print("the second commissioning has ended\n \t\t\tstudnet/\tAbdulmoeen khaled Alshamam");
+//    }}
