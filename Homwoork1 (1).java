9ef96e61f91dbef524f2685b72ab6da0673c7063
abdulmoeen73737 <112026628+abdulmoeen73737@users.noreply.github.com>
2022-09-20 20:04:06 +0300

    Add files via upload

---
+++ Homwoork1 (1).java
@@ -0,0 +1,44 @@
+//اكتب برنامج يطبع تحية من اختيارك
+//public class Hmowoork1.1 {
+//public static void main (String [] args) {
+//	 System.out.println ("Hello   Ali");
+//	}
+//}
+////////////////////////////////////////
+//اكتب رنامج بطبع مجموع اول عشرة اعداد صحيحة
+//public class Hmowoork1.2 {
+//public static void main (String [] args) {
+//	 System.out.println (1+2+3+4+5+6+7+8+9+10);
+//	}
+//}
+////////////////////////////////////////
+//اكتب رنامج بطبع حاصل ضرب اول عشرة اعداد صحيحة
+//public class Hmowoork1.3{
+//public static void main (String [] args) {
+//System.out.println (1*2*3*4*5*6*7*8*9*10);
+//	}
+//}
+////////////////////////////////////////
+//اكتب رنامج بطبع نتيجة العمليات التالية
+//public class Hmowoork1.6 {
+//public static void main (String [] args) {
+	// System.out.println ( 6*8+-5);
+	 //	 System.out.println ((9+55)%9);
+//	 	 	 System.out.println (20+(-3*5)/8);
+//	 System.out.println (5+15/3*2-8%3);
+	 
+//}
+//}
+////////////////////////////////////////////////////
+//    import javax.swing.JOptionPane;
+//    public class Homm {
+//        public static void main(String[] args) {
+//            JOptionPane.showMessageDialog (null,"Hello ,World");
+//        } }
+//////////////////////////////////////////////
+//import javax.swing.JOptionPane;
+//public class Homm {
+//    public static void main(String[] args) {
+//       String name=JOptionPane.showInputDialog("What is your name?");
+//        System.out.println(name);
+//    } }
---
+++ Homwoork3.java
@@ -0,0 +1,116 @@
+public class Homwoork3
+//التكليف الثالث
+//public class Homwoork3 {
+//public static void main(String[] args) {
+//int n = 1;
+//int m = -1;
+//if (n < -m)
+//{ System.out.print(n); }
+//else { System.out.print(m); }
+//	}}
+//يطبع -1
+//public class Homwoork3 {
+//	public static void main(String[] args) {
+//	int n = 1;
+//int m = -1;
+//if (-n >= m)
+//{ System.out.print(n); }
+//else { System.out.print(m); }
+//	}}
+// يطبع  1
+//public class Homwoork3 {
+//	public static void main(String[] args) {
+//double x = 0;
+//double y = 1;
+//if (Math.abs(x - y) < 1)
+//{ System.out.print(x); }
+//else { System.out.print(y); }
+//	}}
+// يطبع 1.0
+//public class Homwoork3 {
+//	public static void main(String[] args) {
+//	double x = Math.sqrt(2);
+//double y = 2;
+//if (x * x == y)
+//{ System.out.print(x); }
+//else { System.out.print(y); }
+//	}}
+// يطبع  2.0
+///////////////////////////////////
+//السؤال الثاني
+//import java.util.Scanner;
+//public class Homwoork3 {
+//    public static void main(String[] args){
+//        int  a;
+//        Scanner in=new Scanner(System.in);
+//        a=in.nextInt();
+//        if(a%2==0)
+//            System.out.print("  1 ");
+//        else
+//            System.out.print(" o");
+//    }}
+////////////////////////////////////
+//السؤال الثالث
+//import java.util.Scanner;
+//public class Homwoork3 {
+//    public static void main(String[] args){
+//        float  a,c,d;
+//        Scanner in=new Scanner(System.in);
+//        a=in.nextInt();
+//        c=in.nextInt();
+//        d=in.nextInt();
+//        if(d==a+c)
+//            System.out.print("   TRue");
+//        else
+//            System.out.print("fals");
+//    }}
+///////////////////////////////////////////////////
+// السؤال الرابع
+//import java.util.Scanner;
+//public class Homwoork3 {
+//    public static void main(String[] args){
+//        int  a;
+//        Scanner in=new Scanner(System.in);
+//        System.out.print("   plies Input the floor : ");
+//        a=in.nextInt();
+//        if(a%a<=0)
+//            System.out.print("  the actual floor to be  :  " );
+//        System.out.println(a-1);
+//    }}
+/////////////////////
+//السؤال الخامس
+//import java.util.Scanner;
+//public class Homwoork3 {
+//    public static void main(String[] args){
+//        float  a,c,d;
+//        Scanner in=new Scanner(System.in);
+//        a=in.nextInt();
+//        c=in.nextInt();
+//        d=in.nextInt();
+//        if(a==c&a==d&d==a&c==d&d==c)
+//            System.out.print(" all the same ");
+//        if(a!=c&a!=d&d!=a&c!=d&d!=c)
+//            System.out.print(" “all different  ");
+//        else
+//            System.out.print("  neither");
+//    }}
+/////////////////////////////
+//السؤال السادس
+
+//////////////////////////////
+//السؤال الاخير
+//package com .company;
+//import java.util.Scanner;
+//public class Homwoork3 {
+//    public static void main(String[] args){
+//        System.out.print(" enter the number   :");
+//        Scanner in=new Scanner(System.in);
+//        int i=in.nextInt();
+//        switch  (i)  {
+//            case 1:System.out.print(" Jun : (  30   )"); break;
+//            case 2:System.out.print(" fabrr : (  28  )"); break;
+//            case 3:System.out.print(" march :  (  30  )"); break;
+//            case 4:System.out.print(" ebril : (  30  )"); break;
+//            case 5:System.out.print(" may :  (  30   )"); break;
+//        }}}
+}
